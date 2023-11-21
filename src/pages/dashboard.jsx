128b1d83f3ba6e3689a32292e9850661035cf01c
joaobarboza <joaolucasbarbozaa@gmail.com>
2023-11-21 01:26:37 -0300

    chore(tasks): tasks marking as completed

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -4,9 +4,13 @@ import { useState } from 'react';
 import { Button } from '../components/ui/button';
 import { Input } from '../components/ui/input';
 import { Separator } from '../components/ui/separator';
+import { Checkbox } from "@/components/ui/checkbox"
+
+
 
 import CardsInfo from '@/components/cards-info';
 import axios from 'axios';
+import { Trash2 } from 'lucide-react';
 
 export default function DashboardPage() {
   const token = sessionStorage.getItem('token');
@@ -14,9 +18,11 @@ export default function DashboardPage() {
   const [userName, setUserName] = useState('');
   const [userEmail, setUserEmail] = useState('');
   const [totalCategories, setTotalCategories] = useState(0);
-  const [taskData, setTaskData] = useState([]);
+  const [taskData, setTaskData] = useState({ tasks: [] });
   const [listData, setListData] = useState([]);
   const [newTaskName, setNewTaskName] = useState('');
+  const [taskCompleted, setTaskCompleted] = useState(0);
+  const [listCompleted, setListCompleted] = useState(0);
 
   const [firstNameInitial, setFirstNameInitial] = useState('');
   const [lastNameInitial, setLastNameInitial] = useState('');
@@ -25,7 +31,7 @@ export default function DashboardPage() {
     const nameParts = name.split(' ');
     const firstNameInitial = nameParts.length > 0 ? nameParts[0].charAt(0) : '';
     const lastNameInitial = nameParts.length > 1 ? nameParts[1].charAt(0) : '';
-    
+
 
     setFirstNameInitial(firstNameInitial);
     setLastNameInitial(lastNameInitial);
@@ -34,7 +40,6 @@ export default function DashboardPage() {
   const config = {
     headers: {
       Authorization: `Bearer ${token}`,
-      // Adicione outros cabeçalhos conforme necessário
     },
   };
 
@@ -42,19 +47,40 @@ export default function DashboardPage() {
     try {
       const newTask = {
         description: newTaskName,
+        completed: false,
       };
-  
+
       await axios.post('http://localhost:3000/tasks', newTask, config);
 
       window.location.reload();
 
       setNewTaskName('');
-  
+
     } catch (error) {
       console.error("Erro ao criar uma nova tarefa:", error);
     }
   };
 
+  const handleTaskCompletion = async (taskId) => {
+    try {
+      await axios.put(`http://localhost:3000/tasks/${taskId}`, null, config);
+
+      setTaskData((prevTaskData) => {
+        const updatedTasks = prevTaskData.tasks.map((task) => {
+          if (task.id === taskId) {
+            return { ...task, completed: true };
+          }
+          return task;
+        });
+
+        setTaskCompleted(updatedTasks.filter((task) => task.completed).length)
+
+        return { ...prevTaskData, tasks: updatedTasks };
+      });
+    } catch (error) {
+      console.error("Erro ao marcar a tarefa como concluída:", error);
+    }
+  };
 
   const fetchUserData = async () => {
     try {
@@ -64,10 +90,6 @@ export default function DashboardPage() {
       console.log("🚀 ~ file: dashboard.jsx:43 ~ fetchUserData ~ tasksResponse:", tasksResponse)
 
       const totalCategoriesResponse = await axios.get('http://localhost:3000/categories', config)
-      
-      
-      console.log("🚀 ~ file: dashboard.jsx:45 ~ fetchUserData ~ totalCategoriesResponse:", totalCategoriesResponse)
-
 
       const listsResponse = await axios.get('http://localhost:3000/todo-lists', config)
       console.log("🚀 ~ file: dashboard.jsx:49 ~ fetchUserData ~ listsResponse:", listsResponse)
@@ -82,7 +104,8 @@ export default function DashboardPage() {
       setTotalCategories(totalCategoriesResponse.data)
       setTaskData(tasksResponse.data)
       setListData(listsResponse.data)
-
+      setTaskCompleted(tasksResponse.data.totalCompleted)
+      setListCompleted(listsResponse.data.totalCompleted)
 
     } catch (error) {
       console.error(error);
@@ -93,8 +116,8 @@ export default function DashboardPage() {
     fetchUserData()
   }, [])
   return (
-    <>
-      <div className="flex-col md:flex bg-[#1A1A1A] h-screen">
+    <div className='bg-[#1A1A1A] flex justify-center'>
+      <div className="flex-col md:flex w-[1200px]">
         <div className="border-b border-[#262626]">
           <div className="flex h-16 items-center px-4">
             <img src="/src/assets/logo.svg" alt="" />
@@ -122,14 +145,14 @@ export default function DashboardPage() {
           />
 
           <Separator className='my-8' />
-          
+
           <div className='flex gap-4 h-16'>
-            <Input 
-              className='h-full' placeholder='Adicione uma nova task' 
+            <Input
+              className='h-full' placeholder='Adicione uma nova task'
               value={newTaskName}
               onChange={(e) => setNewTaskName(e.target.value)}
             />
-            <Button 
+            <Button
               className='h-full w-24 text-lg'
               onClick={handleCreateTask}
             >
@@ -137,8 +160,71 @@ export default function DashboardPage() {
             </Button>
           </div>
 
+          <div className='flex items-start bg pt-8 gap-2 flex-col md:flex-row justify-between' >
+            <div className='flex items-center gap-2 text-xl'>
+              <p className='text-secondary-foreground font-semibold'>Tasks Concluídas</p>
+              <div className='w-10 bg-[#333333] flex justify-center items-center rounded-3xl'>
+                <p className='text-white'>{taskCompleted}</p>
+              </div>
+            </div>
+
+            <div className='flex items-center gap-2 text-xl'>
+              <p className='text-secondary-foreground font-semibold'>Listas Concluídas</p>
+              <div className='w-10 bg-[#333333] flex justify-center items-center rounded-3xl'>
+                <p className='text-white'>{listCompleted}</p>
+              </div>
+            </div>
+          </div>
+
+          {taskData && taskData.tasks && taskData.tasks.slice().reverse().map((task) => (
+
+            task.completed !== true ? (
+              <div key={task.id} className='border border-[#333333] bg-[#262626] h-20 rounded-xl flex justify-start items-center p-4'>
+                <div className="flex justify-between w-full">
+                  <div className='flex items-center space-x-2'>
+                    <Checkbox
+                      id={task.id}
+                      onClick={() => handleTaskCompletion(task.id)}
+                    />
+                    <p
+                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-xl text-[#F2F2F2]"
+                    >
+                      {task.description}
+                    </p>
+                  </div>
+
+                  <Button variant={"ghost"}>
+                    <Trash2 color='#D9D9D9' />
+                  </Button>
+                </div>
+              </div>)
+              :
+              (
+                <div key={task.id} className='border border-[#333333] bg-[#262626] h-20 rounded-xl flex justify-start items-center p-4'>
+                  <div className="flex justify-between w-full">
+                    <div className='flex items-center space-x-2'>
+                      <Checkbox
+                        id={task.id}
+                        checked={task.completed}
+                        onClick={() => handleTaskCompletion(task.id)}
+                      />
+                      <p
+                        className={`text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-xl text-[#F2F2F2] ${task.completed ? 'line-through' : ''
+                          }`}
+                      >
+                        {task.description}
+                      </p>
+                    </div>
+
+                    <Button variant={"ghost"}>
+                      <Trash2 color='#D9D9D9' />
+                    </Button>
+                  </div>
+                </div>
+              )
+          ))}
         </div>
       </div>
-    </>
+    </div>
   )
 }
